832302d114bb6a94b73c33b0fab9ab1f64023c93
sun00215 <sun00215@algonquinlive.com>
2024-02-01 11:00:26 -0500

    first

---
+++ main.js
@@ -0,0 +1,10 @@
+// 1. Declare a variable name and assign it as your name. Print the variable to the console.
+
+
+
+// 2. Add a new fruit to the fruits array using the push method.
+
+// 3. Write a function getDaysUntilAdeshBirthday that calculates and returns the number of days remaining until his birthday. Print the result to the console. Adesh's birthday is on March 25. 
+
+
+
